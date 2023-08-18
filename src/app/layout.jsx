af99f878b8ab0b0f7f9934eff20e50b901ea7919
Manuel De Alba <ferman5903@gmail.com>
2023-08-17 20:28:27 -0600

    Agrega fuente poppins a toda la pagina

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,4 @@
+import { Poppins } from 'next/font/google';
 import "./globals.css";
 import ModalProvider from "@/context/ModalProvider";
 
@@ -6,10 +7,18 @@ export const metadata = {
     description: "Blancos disponibles en StockEssentials",
 };
 
+const poppins = Poppins({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: ["500"],
+    variable: "--font-poppins"
+})
+  
+
 export default function RootLayout({ children }) {
     return (
         <html lang="es">
-            <body>
+            <body className={poppins.variable}>
                 <ModalProvider>{children}</ModalProvider>
             </body>
         </html>
