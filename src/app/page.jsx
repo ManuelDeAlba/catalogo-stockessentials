ce91b25604074ad4570109308e1e44874676c438
Manuel De Alba <ferman5903@gmail.com>
2023-08-17 20:44:33 -0600

    Iconos de redes sociales en el footer para el contacto

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -53,8 +53,19 @@ async function Inicio(){
                 <div className="footer__contenedor contenedor">
                     <h3 className="footer__titulo titulo">Contacto</h3>
                     <div className="footer__links">
-                        <Link className="footer__link" target="_blank" href="https://api.whatsapp.com/send?phone=523327306581">Whatsapp</Link>
-                        <Link className="footer__link" target="_blank" href="https://www.facebook.com/midnaismera.madizonhernandez.1?mibextid=ZbWKwL">Facebook</Link>
+                        <Link className="footer__link" target="_blank" href="https://api.whatsapp.com/send?phone=523327306581">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-whatsapp" width={24} height={24} viewBox="0 0 24 24" stroke-width={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9"></path>
+                                <path d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1"></path>
+                            </svg>
+                        </Link>
+                        <Link className="footer__link" target="_blank" href="https://www.facebook.com/midnaismera.madizonhernandez.1?mibextid=ZbWKwL">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-facebook" width={24} height={24} viewBox="0 0 24 24" stroke-width={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3"></path>
+                            </svg>
+                        </Link>
                     </div>
                 </div>
             </footer>
