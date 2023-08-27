b2566904519854c4198b2754e5bb105599ed5926
Manuel De Alba <ferman5903@gmail.com>
2023-08-26 18:44:01 -0600

    Link con recarga para evitar errores

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,12 +1,10 @@
-import Link from 'next/link';
- 
 export default function NotFound() {
     return (
         <main className='notfound'>
             <section className="notfound__contenedor contenedor">
                 <h1>Home Stock Essentials</h1>
                 <p>No se ha encontrado la página</p>
-                <Link className='boton' href="/">Inicio</Link>
+                <a className='boton' href="/">Inicio</a>
             </section>
         </main>
     )
