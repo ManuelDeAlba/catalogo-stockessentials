b8210fd607d0089da1cd0630b34b82acd76c5ae3
Manuel De Alba <ferman5903@gmail.com>
2023-08-26 22:24:04 -0600

    Redireccion automatica

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,11 +1,13 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function NotFound() {
-    return (
-        <main className='notfound'>
-            <section className="notfound__contenedor contenedor">
-                <h1>Home Stock Essentials</h1>
-                <p>No se ha encontrado la página</p>
-                <a className='boton' href="/">Inicio</a>
-            </section>
-        </main>
-    )
+    const router = useRouter();
+
+    useEffect(() => {
+        router.push("/");
+        router.refresh();
+    }, [])
 }
