4338c82b57932af34ecdf0f71149cc735ed08e46
Manuel De Alba <ferman5903@gmail.com>
2024-08-30 00:07:58 -0600

    Add auto-animate when filter the products

--- src/components/GaleriaProductos.jsx
+++ src/components/GaleriaProductos.jsx
@@ -3,8 +3,13 @@
 import { useState } from "react";
 import Filtro from "./Filtro";
 import Producto from "./Producto";
+import { useAutoAnimate } from '@formkit/auto-animate/react';
+
 function GaleriaProductos({ productos: productosServidor }){
     const [productosFiltrados, setProductosFiltrados] = useState(productosServidor);
+    const [parent, enableAnimations] = useAutoAnimate({
+        duration: 300
+    });
 
     const handleProductosFiltrados = filtrados => {
         setProductosFiltrados(filtrados);
@@ -19,7 +24,7 @@ function GaleriaProductos({ productos: productosServidor }){
                 handleElementosFiltrados={handleProductosFiltrados}
             />
 
-            <section className="productos contenedor">
+            <section className="productos contenedor" ref={parent}>
                 {
                     productosFiltrados.map(producto => (
                         <Producto producto={producto} key={producto.id} />
